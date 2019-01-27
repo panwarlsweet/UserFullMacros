5d4a1a78e35bb565fa1909e5c2aaaca0567c727e
Lata Panwar <panwarlsweet@gmail.com>
2019-01-27 18:03:16 +0530

    corrected

--- batch_script.sh
+++ batch_script.sh
@@ -15,12 +15,11 @@ do
                 break
 	fi
 		echo "#!/bin/sh" >> $job$id.sh
-                echo "export X509_USER_PROXY=/afs/cern.ch/work/l/lata/HH4b/nonreso/DELPHES/x509up_u103989"  >> $job$id.
-sh
+                echo "export X509_USER_PROXY=/afs/cern.ch/work/l/lata/HH4b/nonreso/DELPHES/x509up_u103989"  >> $job$id.sh
 		echo "cd /afs/cern.ch/work/l/lata/HH4b/nonreso/CMSSW_9_1_0_pre3/src" >> $job$id.sh 
 		echo "eval \`scramv1 runtime -sh\`" >> $job$id.sh
 		echo "cd /afs/cern.ch/work/l/lata/HH4b/nonreso/DELPHES/delphes" >> $job$id.sh
-		echo "root -l -b -q BGenfilter_HT1000_1500.C'(200, $n, $m, "\"out_$job$id.root\"")'" >> $job$id.sh    #
+		echo "root -l -b -q BGenfilter_HT1000_1500.C'(200, $n, $m, "\"out_$job$id.root\"")'" >> $job$id.sh    
 ### change .C file
                 n=$m
                 m=`expr $m + 1`
